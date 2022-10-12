96507dd3e1f92e634e34e2d9874f559c9ca97b13
Dipesh Manandhar <dipesh_manz@yahoo.com>
2022-10-11 22:16:08 -0400

    add day 1 solutions

--- src/day1/puzzle1.py
+++ src/day1/puzzle1.py
@@ -1,4 +1,7 @@
 from src.common.read_input import read_input
 
 input = read_input('day1/input1.txt')
-print(input)
+depths = [int(line) for line in input]
+times_increased = sum(map(lambda x, y: y>x, depths, depths[1:]))
+
+print(times_increased)
